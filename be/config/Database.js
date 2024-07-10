3489b34beaa9c265be9b6d13fa8acc83f40b1a10
Samuel Sihotang <samsihotang1@gmail.com>
2024-07-10 11:12:56 +0700

    add route and update env

--- be/config/Database.js
+++ be/config/Database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('auth_db', 'root', 'root', {
-	host: 'localhost',
-	dialect: 'mysql',
-});
+const db = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USERNAME,
+	process.env.DB_PASSWORD,
+	{
+		host: process.env.DB_HOST,
+		dialect: process.env.DB_CONNECTION,
+	}
+);
 
 export default db;
