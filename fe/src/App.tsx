0e03aecc916246aebd96da11cf5d2357f6cda2de
Samuel Sihotang <samsihotang1@gmail.com>
2024-07-10 14:05:44 +0700

    ini lagi

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
+import Account from './pages/Account';
 import Article from './pages/Article';
 import Homepage from './pages/Homepage';
 import Products from './pages/Products';
@@ -17,9 +18,10 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Homepage />} />
-				<Route path="article" element={<Article />} />
-				<Route path="products" element={<Products />} />
-				<Route path="about" element={<About />} />
+				<Route path="/article" element={<Article />} />
+				<Route path="/products" element={<Products />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/account" element={<Account />} />
 				{/* <Route path="edit/:id" element={<EditUser />} /> */}
 			</Routes>
 		</BrowserRouter>
