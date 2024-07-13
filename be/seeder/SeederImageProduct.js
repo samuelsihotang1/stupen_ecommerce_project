01015bbbf1d4499b86013276f5d865d667f2a473
Samuel Sihotang <samsihotang1@gmail.com>
2024-07-13 10:47:01 +0700

    seed product and image

--- be/seeder/SeederImageProduct.js
+++ be/seeder/SeederImageProduct.js
@@ -4,84 +4,184 @@ const SeederImageProduct = async () => {
 	try {
 		const imageProductsData = [
 			{
-				image: 'tas-belanja.jpg',
+				image: 'tas-belanja-ramah-lingkungan.jfif',
 				productId: 1,
 			},
 			{
-				image: 'tas-belanja2.jpg',
+				image: 'tas-belanja-ramah-lingkungan2.jfif',
 				productId: 1,
 			},
 			{
-				image: 'botol-minum.jpg',
+				image: 'tas-belanja-ramah-lingkungan3.jfif',
+				productId: 1,
+			},
+			{
+				image: 'tas-belanja-ramah-lingkungan4.jfif',
+				productId: 1,
+			},
+			{
+				image: 'gantungan-kunci-kertas-daur-ulang.jfif',
 				productId: 2,
 			},
 			{
-				image: 'botol-minum2.jpg',
+				image: 'gantungan-kunci-kertas-daur-ulang2.jfif',
 				productId: 2,
 			},
 			{
-				image: 'wadah-makanan.jpg',
+				image: 'gantungan-kunci-kertas-daur-ulang3.jfif',
+				productId: 2,
+			},
+			{
+				image: 'gantungan-kunci-kertas-daur-ulang4.jfif',
+				productId: 2,
+			},
+			{
+				image: 'gaun-indah-dengan-kertas-daur-ulang.jfif',
 				productId: 3,
 			},
 			{
-				image: 'wadah-makanan2.jpg',
+				image: 'gaun-indah-dengan-kertas-daur-ulang2.jfif',
 				productId: 3,
 			},
 			{
-				image: 'mainan-anak.jpg',
+				image: 'gaun-indah-dengan-kertas-daur-ulang3.jfif',
+				productId: 3,
+			},
+			{
+				image: 'gaun-indah-dengan-kertas-daur-ulang4.jfif',
+				productId: 3,
+			},
+			{
+				image: 'mainan-anak-dari-plastik-daur-ulang.jfif',
 				productId: 4,
 			},
 			{
-				image: 'mainan-anak2.jpg',
+				image: 'mainan-anak-dari-plastik-daur-ulang2.jfif',
 				productId: 4,
 			},
 			{
-				image: 'meja-taman.jpg',
+				image: 'mainan-anak-dari-plastik-daur-ulang3.jfif',
+				productId: 4,
+			},
+			{
+				image: 'mainan-anak-dari-plastik-daur-ulang4.jfif',
+				productId: 4,
+			},
+			{
+				image: 'mobil-mainan-anak-dari-kardus-daur-ulang.jfif',
 				productId: 5,
 			},
 			{
-				image: 'meja-taman2.jpg',
+				image: 'mobil-mainan-anak-dari-kardus-daur-ulang2.jfif',
 				productId: 5,
 			},
 			{
-				image: 'kerajinan-tangan.jpg',
+				image: 'mobil-mainan-anak-dari-kardus-daur-ulang3.jfif',
+				productId: 5,
+			},
+			{
+				image: 'mobil-mainan-anak-dari-kardus-daur-ulang4.jfif',
+				productId: 5,
+			},
+			{
+				image: 'kerajinan-tangan-dari-kertas-daur-ulang.jfif',
 				productId: 6,
 			},
 			{
-				image: 'kerajinan-tangan2.jpg',
+				image: 'kerajinan-tangan-dari-kertas-daur-ulang2.jfif',
 				productId: 6,
 			},
 			{
-				image: 'lampu-hias2.jpg',
+				image: 'kerajinan-tangan-dari-kertas-daur-ulang3.jfif',
+				productId: 6,
+			},
+			{
+				image: 'kerajinan-tangan-dari-kertas-daur-ulang4.jfif',
+				productId: 6,
+			},
+			{
+				image: 'lampu-hias-dari-botol-plastik-daur-ulang.jfif',
 				productId: 7,
 			},
 			{
-				image: 'lampu-hias.jpg',
+				image: 'lampu-hias-dari-botol-plastik-daur-ulang2.jfif',
 				productId: 7,
 			},
 			{
-				image: 'notebook2.jpg',
+				image: 'lampu-hias-dari-botol-plastik-daur-ulang3.jfif',
+				productId: 7,
+			},
+			{
+				image: 'vas-bunga-dari-kertas-daur-ulang.jfif',
 				productId: 8,
 			},
 			{
-				image: 'notebook.jpg',
+				image: 'vas-bunga-dari-kertas-daur-ulang2.jfif',
 				productId: 8,
 			},
 			{
-				image: 'vas-bunga2.jpg',
+				image: 'vas-bunga-dari-kertas-daur-ulang3.jfif',
+				productId: 8,
+			},
+			{
+				image: 'vas-bunga-dari-kertas-daur-ulang4.jfif',
+				productId: 8,
+			},
+			{
+				image: 'gelas-unik-dari-bambu.jfif',
 				productId: 9,
 			},
 			{
-				image: 'vas-bunga.jpg',
+				image: 'gelas-unik-dari-bambu2.jfif',
 				productId: 9,
 			},
 			{
-				image: 'dompet2.jpg',
+				image: 'teko-tradisional-dari-kelapa.jfif',
 				productId: 10,
 			},
 			{
-				image: 'dompet.jpg',
+				image: 'teko-tradisional-dari-kelapa2.jfif',
 				productId: 10,
+			},
+			{
+				image: 'teko-tradisional-dari-kelapa3.jfif',
+				productId: 10,
+			},
+			{
+				image: 'teko-tradisional-dari-kelapa4.jfif',
+				productId: 10,
+			},
+			{
+				image: 'mangkok-tempurung-kelapa-berukir.jfif',
+				productId: 11,
+			},
+			{
+				image: 'mangkok-tempurung-kelapa-berukir2.jfif',
+				productId: 11,
+			},
+			{
+				image: 'mangkok-tempurung-kelapa-berukir3.jfif',
+				productId: 11,
+			},
+			{
+				image: 'mangkok-tempurung-kelapa-berukir4.jfif',
+				productId: 11,
+			},
+			{
+				image: 'tempat-atk-ukir-dari-bambu.jfif',
+				productId: 12,
+			},
+			{
+				image: 'tempat-atk-ukir-dari-bambu2.jfif',
+				productId: 12,
+			},
+			{
+				image: 'tempat-atk-ukir-dari-bambu3.jfif',
+				productId: 12,
+			},
+			{
+				image: 'tempat-atk-ukir-dari-bambu4.jfif',
+				productId: 12,
 			},
 		];
 
